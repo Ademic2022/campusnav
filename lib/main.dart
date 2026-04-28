1cc7f30dfb495740df4931e07e82066125a545a7
Ademic2022 <adeeyomichael77@gmail.com>
2026-04-28 16:48:45 +0100

    feat: integrate flutter_dotenv for environment variable management and update .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,10 @@ import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'core/models/saved_location.dart';
 import 'core/services/storage_service.dart';
 import 'app.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// ⚠️  Replace this with your actual Mapbox public token.
 /// Get one free at https://account.mapbox.com
-final String _mapboxToken = dotenv.env['MAPBOX_TOKEN'] ?? '';
+const String _mapboxToken = '';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
